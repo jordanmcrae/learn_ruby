4122983650ad78d341bef54fa292c53327b642e5
Jordan <jordanmcrae@hotmail.com>
2015-04-14 15:53:01 -0400

    One error in 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,46 @@
-aa
+def echo(string)
+  string
+end
+
+def shout(string)
+  string.upcase
+end
+
+def repeat(string, num = 2)
+  # ((string * num)
+  results = []
+  num.times do
+    results << string
+  end
+  results.join(" ")
+end
+
+def start_of_word(string, num)
+  string[0, num]
+end
+
+def first_word(string)
+  string.split[0]
+end
+
+def titleize(string)
+  ignore_these_words = ["and", "or", "if", "the"]
+  string.split(' ').each{ |i| i.capitalize! if ! ignore_these_words.include? i }.join(' ')
+end
+
+
+
+
+
+
+# def start_of_words(string, num)
+#   strings.split(num)
+# end
+
+# def repeat(string, num)
+#   num.times do
+#     puts string
+#   end
+# end
+
+
